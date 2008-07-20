9fe5e2fc7d716e776d01b6de16e16eb363383c07
Luke Bayes <lbayes@patternpark.com>
2008-07-19 17:54:44 -0700

    Incremented the revision for the mxml bundle

--- bundles/mxml/lib/sprout/mxml/version.rb
+++ bundles/mxml/lib/sprout/mxml/version.rb
@@ -3,7 +3,7 @@ module Sprout # :nodoc:
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 1
-      TINY  = 10
+      TINY  = 11
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       MAJOR_MINOR = [MAJOR, MINOR].join('.')
