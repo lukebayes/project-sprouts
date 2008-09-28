27577afc68e230abd771a012ad9421591ff8d064
Nathan Fisher <nfisher@junctionbox.ca>
2008-09-28 15:40:45 -0400

    bundles/as3/test/asdoc_test.rb:
    	* Extracted direct_configuration method from test_configure_directly method.
    	* Added test_exclude_expressions to test new parameter.
    	* Modified assertion in test_configure_with_source_paths to use $HOME instead of fixed /Users/lbayes path.

--- bundles/as3/test/asdoc_test.rb
+++ bundles/as3/test/asdoc_test.rb
@@ -26,14 +26,7 @@ class AsDocTest <  Test::Unit::TestCase
   end
 
   def test_configure_directly
-    t = asdoc :docs do |t|
-      t.doc_sources << @asunit
-      t.doc_sources << @src
-      t.doc_sources << @test
-      # Need to force templates_path so that these tests run on different machines
-      t.templates_path << 'foo'
-    end
-    
+    t = direct_configuration
     assert_equal('-doc-sources+=lib/asunit -doc-sources+=src -doc-sources+=test -output=doc -templates-path+=foo', t.to_shell)
   end
 
@@ -59,7 +52,7 @@ class AsDocTest <  Test::Unit::TestCase
       t.source_path << 'src'
     end
     
-    assert_equal('-output=doc -source-path+=src -templates-path+=/Users/lbayes/Library/Sprouts/cache/0.7/sprout-flex3sdk-tool-3.0.2/archive/asdoc/templates', t.to_shell)
+    assert_equal("-output=doc -source-path+=src -templates-path+=#{ENV['HOME']}/Library/Sprouts/cache/0.7/sprout-flex3sdk-tool-3.0.2/archive/asdoc/templates", t.to_shell)
   end
   
   def test_automatic_template_path
@@ -67,5 +60,26 @@ class AsDocTest <  Test::Unit::TestCase
 
     assert(t.to_shell.match(/-templates-path\+=.*\/Sprouts\/cache\//))
   end
+
+	def test_exclude_expressions
+		t = direct_configuration
+		t.exclude_expressions << 'lib/asunit/**/*'
+		t.exclude_expressions << 'test/**/*'
+
+		excluded_classes = %w{ asunit.asunit.util.Properties asunit.asunit.util.Iterator asunit.asunit.util.ArrayIterator asunit.asunit.textui.XMLResultPrinter asunit.asunit.textui.TestRunner asunit.asunit.textui.ResultPrinter asunit.asunit.textui.FlexTestRunner asunit.asunit.textui.FlexRunner asunit.asunit.runner.Version asunit.asunit.runner.TestSuiteLoader asunit.asunit.runner.BaseTestRunner asunit.asunit.framework.TestSuite asunit.asunit.framework.TestResult asunit.asunit.framework.TestMethod asunit.asunit.framework.TestListener asunit.asunit.framework.TestFailure asunit.asunit.framework.TestCaseExample asunit.asunit.framework.TestCase asunit.asunit.framework.Test asunit.asunit.framework.RemotingTestCase asunit.asunit.framework.AsynchronousTestCaseExample asunit.asunit.framework.AsynchronousTestCase asunit.asunit.framework.Assert asunit.asunit.errors.UnimplementedFeatureError asunit.asunit.errors.InstanceNotFoundError asunit.asunit.errors.ClassNotFoundError asunit.asunit.errors.AssertionFailedError asunit.asunit.errors.AbstractMemberCalledError utils.MathUtilTest AllTests }.join( " -exclude-classes=" )
+    assert_equal("-doc-sources+=lib/asunit -doc-sources+=src -doc-sources+=test -library-path+=lib -output=doc -templates-path+=foo -exclude-classes=#{excluded_classes}", t.to_shell)
+	end
+
+	private
+
+	def direct_configuration
+		asdoc :docs do |t|
+      t.doc_sources << @asunit
+      t.doc_sources << @src
+      t.doc_sources << @test
+      # Need to force templates_path so that these tests run on different machines
+      t.templates_path << 'foo'
+    end
+	end
 
 end
