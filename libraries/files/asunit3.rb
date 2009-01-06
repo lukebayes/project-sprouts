a08e58dd8339addc6c4214089d4ada7d72e3a31b
Luke Bayes <lbayes@patternpark.com>
2009-01-06 12:00:53 -0800

    Updated asunit library to pull latest revision

--- libraries/files/asunit3.rb
+++ libraries/files/asunit3.rb
@@ -5,7 +5,7 @@
 name = File.basename(__FILE__).split('.').shift
 
 gem_wrap name do |t|
-  t.version       = '3.2.5'
+  t.version       = '3.2.6'
   t.summary       = "AsUnit3 is an ActionScript unit test framework for AIR, Flex 2/3 and ActionScript 3 projects"
   t.author        = "Luke Bayes and Ali Mills"
   t.email         = "projectsprouts@googlegroups.com"
@@ -13,8 +13,8 @@ gem_wrap name do |t|
   t.sprout_spec   =<<EOF
 - !ruby/object:Sprout::RemoteFileTarget 
   platform: universal
-  url: http://projectsprouts.googlecode.com/files/asunit3-1.5.zip
-  md5: 7eca594553fc9613523f554b8a1a6a5c
+  url: http://projectsprouts.googlecode.com/files/asunit3-1.6.zip
+  md5: 7f7181c7e34087ec6d5aef806867eec5
   archive_path: ''
 EOF
 end
