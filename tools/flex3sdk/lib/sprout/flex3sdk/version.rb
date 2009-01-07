f96b929ce6e9a9672aaa1f7ec41b076e5062ce35
Luke Bayes <lbayes@patternpark.com>
2009-01-07 10:38:30 -0800

    Updated Flex3sdk version number to reflect new download

--- tools/flex3sdk/lib/sprout/flex3sdk/version.rb
+++ tools/flex3sdk/lib/sprout/flex3sdk/version.rb
@@ -2,8 +2,8 @@ module Sprout
   class Flex3SDK #:nodoc:
     module VERSION #:nodoc:
       MAJOR = 3
-      MINOR = 0
-      TINY  = 3
+      MINOR = 2
+      TINY  = 0
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       MAJOR_MINOR = [MAJOR, MINOR].join('.')
