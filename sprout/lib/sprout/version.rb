b0b93f0ae7c9069f9affbea41d118ee9f788121b
Luke Bayes <lbayes@patternpark.com>
2009-04-18 15:20:32 -0700

    Incremented revision number

--- sprout/lib/sprout/version.rb
+++ sprout/lib/sprout/version.rb
@@ -3,7 +3,7 @@ module Sprout
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 7
-      TINY  = 206
+      TINY  = 207
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       MAJOR_MINOR = [MAJOR, MINOR].join('.')
