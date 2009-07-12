e941087afdb70abfdc15ba14c354a31793cbeceb
Luke Bayes <lbayes@patternpark.com>
2009-07-12 12:41:26 -0700

    Incremented revision number

--- libraries/files/asunit3.rb
+++ libraries/files/asunit3.rb
@@ -5,7 +5,7 @@
 name = File.basename(__FILE__).split('.').shift
 
 gem_wrap name do |t|
-  t.version       = '4.0.9'
+  t.version       = '4.0.10'
   t.summary       = "AsUnit3 is an ActionScript unit test framework for AIR, Flex 2/3 and ActionScript 3 projects"
   t.author        = "Luke Bayes and Ali Mills"
   t.email         = "projectsprouts@googlegroups.com"
@@ -15,7 +15,8 @@ gem_wrap name do |t|
   platform: universal
   filename: asunit3.zip
   library_path: asunit3
-  url: http://download.github.com/lukebayes-asunit-004c1dcef1634a1807ed24066801b874dd156234.zip
+  archive_type: zip
+  url: http://github.com/lukebayes/asunit/zipball/4.0.1
   md5: cf3df9b9f0d9c89a500fef0d681e46fc
   archive_path: 'lukebayes-asunit-004c1dcef1634a1807ed24066801b874dd156234/as3/src'
 EOF
