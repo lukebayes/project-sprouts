b4a213df134fc67eb18f05c84c0ccfabea41e3e2
Luke Bayes <lbayes@patternpark.com>
2009-07-24 11:19:20 -0700

    Incremented Revision #

--- bundles/mxml/lib/sprout/mxml/version.rb
+++ bundles/mxml/lib/sprout/mxml/version.rb
@@ -3,7 +3,7 @@ module Sprout # :nodoc:
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 1
-      TINY  = 11
+      TINY  = 12
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       MAJOR_MINOR = [MAJOR, MINOR].join('.')
