ebbc4c0903890f83a438bdcc3b2d17367155ec07
lukebayes <lbayes@patternpark.com>
2009-10-01 19:34:32 -0700

    Incremented sprout gem version number

--- sprout/lib/sprout/version.rb
+++ sprout/lib/sprout/version.rb
@@ -3,7 +3,7 @@ module Sprout
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 7
-      TINY  = 213
+      TINY  = 214
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       MAJOR_MINOR = [MAJOR, MINOR].join('.')
