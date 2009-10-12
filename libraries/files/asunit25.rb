b24c03329c011079b2c106c3e3c0ec5c7b5d5647
lukebayes <lbayes@patternpark.com>
2009-10-12 10:59:57 -0700

    Fixed invalid asunit25 gem

--- libraries/files/asunit25.rb
+++ libraries/files/asunit25.rb
@@ -5,7 +5,7 @@
 name = File.basename(__FILE__).split('.').shift
 
 gem_wrap name do |t|
-  t.version       = '2.2.2'
+  t.version       = '2.2.3'
   t.summary       = "AsUnit25 is an ActionScript unit test framework for Flash Player 7 and 8 projects"
   t.author        = "Luke Bayes and Ali Mills"
   t.email         = "projectsprouts@googlegroups.com"
@@ -13,6 +13,10 @@ gem_wrap name do |t|
   t.sprout_spec   =<<EOF
 - !ruby/object:Sprout::RemoteFileTarget 
   platform: universal
+  filename: asunit3.zip
+  library_path: asunit3
+  archive_type: zip
+  url: http://github.com/lukebayes/asunit/zipball/4.0.3
   md5: 243c963f4d7191081ae200600aa698cb
   archive_path: 'lukebayes-asunit-e2af845/as25/src'
 EOF
