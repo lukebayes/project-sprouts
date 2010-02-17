dbb680c8617e5678b00ffcde564e7e1462b1b491
Hannes Horneber <hannes.horneber@takuma.de>
2010-02-17 13:00:54 +0100

    updated version file

--- bundles/air/lib/sprout/air/version.rb
+++ bundles/air/lib/sprout/air/version.rb
@@ -3,7 +3,7 @@ module Sprout # :nodoc:
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 0
-      TINY  = 1
+      TINY  = 2
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       MAJOR_MINOR = [MAJOR, MINOR].join('.')
