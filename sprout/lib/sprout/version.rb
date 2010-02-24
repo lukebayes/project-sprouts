e67c2ebf0c1d406c4d59d5b794343e7d925f8de9
lukebayes <lbayes@patternpark.com>
2010-02-23 17:43:44 -0800

    Added solaris to list of known Unix platforms

--- sprout/lib/sprout/version.rb
+++ sprout/lib/sprout/version.rb
@@ -3,7 +3,7 @@ module Sprout
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 7
-      TINY  = 224
+      TINY  = 225
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       MAJOR_MINOR = [MAJOR, MINOR].join('.')
