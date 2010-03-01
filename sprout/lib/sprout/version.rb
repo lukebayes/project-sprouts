c6a6c2c1873d606a49945ca1d596b5aabc36ee39
lukebayes <lbayes@patternpark.com>
2010-03-01 14:52:29 -0800

    Incremented version number

--- sprout/lib/sprout/version.rb
+++ sprout/lib/sprout/version.rb
@@ -3,7 +3,7 @@ module Sprout
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 7
-      TINY  = 229
+      TINY  = 230
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       MAJOR_MINOR = [MAJOR, MINOR].join('.')
