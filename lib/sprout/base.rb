a7c09f9998c6fd83223d8dbe085280b7582e246a
lukebayes <lbayes@patternpark.com>
2010-04-27 23:28:35 -0700

    Renamed errors.rb to exceptions.rb - more similar to RubyGems

--- lib/sprout/base.rb
+++ lib/sprout/base.rb
@@ -2,7 +2,7 @@ require 'rake'
 
 # Core, Crossplatform support:
 require 'sprout/log'
-require 'sprout/errors'
+require 'sprout/exceptions'
 require 'sprout/platform'
 require 'sprout/process_runner'
 require 'sprout/user'
