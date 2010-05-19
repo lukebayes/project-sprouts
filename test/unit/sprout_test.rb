8d98561474d8cd41330f17dd0dbc930c9706dde5
lukebayes <lbayes@patternpark.com>
2010-05-18 23:15:50 -0700

    Removed duplicate error test

--- test/unit/sprout_test.rb
+++ test/unit/sprout_test.rb
@@ -15,7 +15,6 @@ class SproutTest < Test::Unit::TestCase
       ProcessRunnerError,
       SproutError, 
       ToolError,
-      ToolError,
       UnknownArchiveType,
       UsageError,
       VersionRequirementNotMetError
