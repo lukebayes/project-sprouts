ad3b4d210bfeb868b8c6e1d8d13274afc0ab1e39
Kristofer Joseph <kristofer@kristoferjoseph.com>
2010-06-15 08:52:52 -0700

    Added logic and tests for adding a namespace to a generator. Added todos to the library creator to add a lib dir and a vendor dir.

--- lib/generators/generator/generator_generator.rb
+++ lib/generators/generator/generator_generator.rb
@@ -2,24 +2,29 @@
 module Sprout
   class GeneratorGenerator < Generator::Base
 
-    add_param :lib, String, { :default => "lib" }
-    add_param :generators, String, { :default => "generators" }
-    add_param :test, String, { :default => "test" }
-    add_param :unit, String, { :default => "unit" }
-    add_param :fixtures, String, { :default => "fixtures" }
-    add_param :bin, String, { :default => "bin" }
-    add_param :extension, String, { :default => ".as" }
+    add_param :namespace, String, { :default  => ''}
+    add_param :lib, String, { :default => 'lib' }
+    add_param :generators, String, { :default => 'generators' }
+    add_param :test, String, { :default => 'test' }
+    add_param :unit, String, { :default => 'unit' }
+    add_param :fixtures, String, { :default => 'fixtures' }
+    add_param :vendor, String, { :default => 'vendor' }    
+    add_param :bin, String, { :default => 'bin' }
+    add_param :extension, String, { :default => '.as' }
 
-    def manifest
+    def manifest  
       directory bin do
         template "#{input.snake_case}", "generator_executable"
       end
 
       directory lib do
-        directory generators do
-          template "#{input.snake_case}_generator.rb", "generator_class.rb"
-          directory "templates" do
-            template "#{input.camel_case}#{extension}", "generator_template"
+        #We need to add a folder with the same name as the module to be used in order to faux namespace our generators to avoid collisions from super classes
+        directory namespace do
+          directory generators do
+            template "#{input.snake_case}_generator.rb", "generator_class.rb"
+            directory "templates" do
+              template "#{input.camel_case}#{extension}", "generator_template"
+            end
           end
         end
       end
@@ -33,6 +38,9 @@ module Sprout
           directory "generators"
         end
       end
+      
+      #This should actually be moved to the library generator
+      #directory vendor
 
     end
 
--- lib/generators/library/library_generator.rb
+++ lib/generators/library/library_generator.rb
@@ -1,7 +1,11 @@
 
 module Sprout
   class LibraryGenerator < Generator::Base
-
+    #TODO: The library generator needs to:
+    #Create the outer folder with the input passed in as the library name if no outer folder exists
+    #Create a lib dir and put the file generated from the library.rb template in it
+    #Create a vendor folder
+    
     ##
     # Set the version string to use.
     add_param :version, String, { :default => '0.0.1' }
