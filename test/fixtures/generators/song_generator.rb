5a3c435ba240184c7a1c2039785a0bc27cb19d7e
lukebayes <lbayes@patternpark.com>
2010-06-20 13:59:29 -0700

    Added comments to a fixture for easier sharing

--- test/fixtures/generators/song_generator.rb
+++ test/fixtures/generators/song_generator.rb
@@ -1,11 +1,20 @@
 
 class SongGenerator < Sprout::Generator::Base
-
+  ##
+  # Set your favorite song name:
   add_param :favorite, String, { :default => 'Emerge' }
 
+  ##
+  # Define your generator directories, files and templates:
   def manifest
-    template "#{favorite.gsub(' ', '').snake_case}.txt", 'Song.txt'
+    template "#{favorite_cleaned}.txt", 'Song.txt'
   end
 
+  protected
+
+  # helper methods will be available to templates too:
+  def favorite_cleaned
+    favorite.gsub(' ', '').snake_case
+  end
 end
 
