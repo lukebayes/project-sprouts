196bdceebfae03470a5c040da71f4e9c47e470fb
lukebayes <lbayes@patternpark.com>
2010-06-22 08:46:45 -0700

    Updated AsUnit packages

--- libraries/files/asunit25.rb
+++ libraries/files/asunit25.rb
@@ -5,7 +5,7 @@
 name = File.basename(__FILE__).split('.').shift
 
 gem_wrap name do |t|
-  t.version       = '2.2.6'
+  t.version       = '2.2.7'
   t.summary       = "AsUnit25 is an ActionScript unit test framework for Flash Player 7 and 8 projects"
   t.author        = "Luke Bayes and Ali Mills"
   t.email         = "projectsprouts@googlegroups.com"
@@ -17,7 +17,7 @@ gem_wrap name do |t|
   library_path: asunit3
   archive_type: zip
   url: http://github.com/lukebayes/asunit/zipball/4.0.3
-  md5: 51fa35e79e2d4349d0e0d44652fb00cb
-  archive_path: 'lukebayes-asunit-4.0.3-0-ge2af845/as2/src'
+  md5: 243c963f4d7191081ae200600aa698cb
+  archive_path: 'lukebayes-asunit-e2af845/as25/src'
 EOF
 end
