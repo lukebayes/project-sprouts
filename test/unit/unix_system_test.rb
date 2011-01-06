58b142512ae5563ee687fd33700b49e09a356316
Clint Modien <cmodien@gmail.com>
2011-01-05 19:48:45 -0800

    changing crlf to lf

--- test/unit/unix_system_test.rb
+++ test/unit/unix_system_test.rb
@@ -33,9 +33,11 @@ class UnixSystemTest < Test::Unit::TestCase
     context "when fed an application with windows line endings" do
 
       setup do
-        @source = File.join fixtures, 'executable', 'windows_line_endings'
+        exe_with_crlf = "#!/bin/sh\r\n\r\n################################################################################\r\n##\r\n##  ADOBE SYSTEMS INCORPORATED\r\n##  Copyright 2007 Adobe Systems Incorporated\r\n##  All Rights Reserved.\r\n##\r\n##  NOTICE: Adobe permits you to use, modify, and distribute this file\r\n##  in accordance with the terms of the license agreement accompanying it.\r\n##\r\n################################################################################\r\n\r\n#\r\n# mxmlc launch script for unix.  On windows, mxmlc.exe is used and\r\n# java settings are managed in jvm.config in this directory.\r\n#\r\n\r\ncase `uname` in\r\n		CYGWIN*)\r\n			OS=\"Windows\"\r\n		;;\r\n		*)\r\n			OS=Unix\r\nesac\r\n\r\nif [ $OS = \"Windows\" ]; then\r\n	# set FLEX_HOME relative to mxmlc if not set\r\n	test \"$FLEX_HOME\" = \"\" && {\r\n	FLEX_HOME=`dirname $0`/..\r\n    	FLEX_HOME=`cygpath -m $FLEX_HOME`\r\n	}\r\n\r\nelif [ $OS = \"Unix\" ]; then\r\n\r\n	# set FLEX_HOME relative to mxmlc if not set\r\n	test \"$FLEX_HOME\" = \"\" && {\r\n	FLEX_HOME=`dirname \"$0\"`/..\r\n	}\r\n\r\nfi\r\n\r\n# don't use $FLEX_HOME in this variable because it may contain spaces,\r\n# instead put it on the java args directly, with double-quotes around it\r\nVMARGS=\"-Xmx384m -Dsun.io.useCanonCaches=false\"\r\n\r\njava $VMARGS -jar \"$FLEX_HOME/lib/mxmlc.jar\" +flexlib=\"$FLEX_HOME/frameworks\" \"$@\"\r\n"
         @target = File.join fixtures, 'executable', 'windows_line_endings.tmp'
-        FileUtils.cp @source, @target
+        File.open(@target, 'wb+') do |f|
+          f.write exe_with_crlf
+        end
       end
 
       teardown do
@@ -49,4 +51,5 @@ class UnixSystemTest < Test::Unit::TestCase
     end
   end
 end
+
 
