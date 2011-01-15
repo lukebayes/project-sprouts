ab3ba312a5fe59fd094aa67bb78e380c6b0e69ca
lukebayes <lbayes@patternpark.com>
2011-01-14 18:05:24 -0800

    Silenced the session test case

--- test/unit/executable_session_test.rb
+++ test/unit/executable_session_test.rb
@@ -8,30 +8,30 @@ class ExecutableSessionTest < Test::Unit::TestCase
 
     setup do
       # Uncomment the following to see interactive sessions:
-      Sprout.stdout = $stdout
-      Sprout.stderr = $stderr
+      #Sprout.stdout = $stdout
+      #Sprout.stderr = $stderr
     end
 
     should "execute without shell params" do
       @fdb = Sprout::FDB.new
       # Comment to hit real FDB:
-      #@fdb.binary_path = File.join fixtures, 'executable', 'flex3sdk_gem', 'fdb'
+      @fdb.binary_path = File.join fixtures, 'executable', 'flex3sdk_gem', 'fdb'
 
       @fdb.execute false
       @fdb.run
  
       # Uncomment if you are on OSX and want to 
       # test the real FDB while running a real SWF:
-      Kernel.system 'open ~/Projects/Sprouts/flashsdk/test/fixtures/flashplayer/AsUnit\ Runner.swf'
-      @fdb.wait_for_prompt
+      #Kernel.system 'open ~/Projects/Sprouts/flashsdk/test/fixtures/flashplayer/AsUnit\ Runner.swf'
+      #@fdb.wait_for_prompt
       
       @fdb.break "AsUnitRunner:12"
 
       @fdb.continue
       @fdb.continue
 
-      @fdb.handle_user_input
-      #@fdb.quit
+      #@fdb.handle_user_input
+      @fdb.quit
     end
 
   end
