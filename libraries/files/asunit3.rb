a2326ac1393424f2e0eae857f4b125df196be78b
lukebayes <lbayes@patternpark.com>
2011-01-21 12:11:57 -0800

    Fixed github url now that redirects work properly

--- libraries/files/asunit3.rb
+++ libraries/files/asunit3.rb
@@ -5,7 +5,7 @@
 name = File.basename(__FILE__).split('.').shift
 
 gem_wrap name do |t|
-  t.version       = '4.0.20'
+  t.version       = '4.0.21'
   t.summary       = "AsUnit3 is an ActionScript unit test framework for AIR, Flex 2/3 and ActionScript 3 projects"
   t.author        = "Luke Bayes and Ali Mills"
   t.email         = "projectsprouts@googlegroups.com"
@@ -16,7 +16,7 @@ gem_wrap name do |t|
   filename: asunit3.zip
   library_path: asunit3
   archive_type: zip
-  url: https://download.github.com/patternpark-asunit-4.0.3-0-ge2af845.zip
+  url: https://github.com/patternpark/asunit/zipball/4.0.3
   md5: 9fa5c9e95a34319d2c7851c6de489d65
   archive_path: 'patternpark-asunit-e2af845/as3/src'
 EOF
